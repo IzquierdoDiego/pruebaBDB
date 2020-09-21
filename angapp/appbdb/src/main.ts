66fb4f172dbd903c6c24e5f6160bd87bf37eb6eb
Diego <izquierdodiegof@gmail.com>
2020-09-20 22:35:54 -0500

    form ang

--- angapp/appbdb/src/main.ts
+++ angapp/appbdb/src/main.ts
@@ -2,6 +2,9 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { environment } from './environments/environment';
+import { FormGroup, FormControl } from '@angular/forms';
+import { error } from '@angular/compiler/src/util';
+import { Router } from '@angular/router';
 
 if (environment.production) {
   enableProdMode();
